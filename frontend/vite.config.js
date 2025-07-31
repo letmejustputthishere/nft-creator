34f6d23f446a4d5b4ea93d0c5f9d05963640584f
Moritz Fuller <32162112+letmejustputthishere@users.noreply.github.com>
2025-07-31 09:53:17 +0200

    Initial commit

---
+++ frontend/vite.config.js
@@ -0,0 +1,39 @@
+import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite';
+import { fileURLToPath, URL } from 'url';
+import environment from 'vite-plugin-environment';
+import tailwindcss from '@tailwindcss/vite'
+
+
+export default defineConfig({
+  base: './',
+  plugins: [react(), environment('all', { prefix: 'CANISTER_' }), environment('all', { prefix: 'DFX_' }), tailwindcss()],
+  envDir: '../',
+  define: {
+    'process.env': process.env
+  },
+  optimizeDeps: {
+    esbuildOptions: {
+      define: {
+        global: 'globalThis'
+      }
+    }
+  },
+  resolve: {
+    alias: [
+      {
+        find: 'declarations',
+        replacement: fileURLToPath(new URL('../src/declarations', import.meta.url))
+      }
+    ]
+  },
+  server: {
+    proxy: {
+      '/api': {
+        target: 'http://127.0.0.1:4943',
+        changeOrigin: true
+      }
+    },
+    host: '127.0.0.1'
+  }
+});
